09766c8275afc85b2a001a6cc7156be8439660ea
ian4727 <elaine_gervacio@yahoo.com>
2023-12-19 19:49:03 +0800

    Updating dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+export default {
+  base: '/portfolio/',
+};
+
